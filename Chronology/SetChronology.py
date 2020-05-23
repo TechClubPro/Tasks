9c88a3ea03262c4860a2b20ee9625977b15132d1
TechClubPro <62166836+TechClubPro@users.noreply.github.com>
2020-05-23 10:29:33 +0530

    Add files via upload

---
+++ Chronology/SetChronology.py
@@ -0,0 +1,24 @@
+# Display Module
+
+print("1. Alexander's invasion of India")
+print("2. Mahajanapada Period ")
+print("3. Life of Gautam Buddha")
+print("4. Battle of Kalinga")
+print("5. Ashoka's riegn")
+
+#Correct Sequence 2 1 3 5 4
+correctSequence ="2 1 3 5 4"
+#User Input Module
+print("Sequence Answer Format : write a number and give space to write 2nd number:")
+print("for e.g. 1 2 3 4 5")
+sequence = input("Please input the sequence of Events : ")
+
+#Comparison & Display Result
+
+if correctSequence == sequence:
+    print("Corect Chronology!! You know your History very well")
+    
+else:
+    print("Start Studying your History!!")
+    
+
